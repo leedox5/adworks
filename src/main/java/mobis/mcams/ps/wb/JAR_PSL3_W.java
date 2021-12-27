7f388566e722f79535a56a397ffea178a066acc9
Myungho Lee <leedox@naver.com>
2021-12-27 12:14:54 +0900

    SYNC

---
+++ src/main/java/mobis/mcams/ps/wb/JAR_PSL3_W.java
@@ -0,0 +1,13 @@
+package mobis.mcams.ps.wb;
+
+public class JAR_PSL3_W {
+
+	public boolean isValidOTP(String userId, String randomNo) {
+		return false;
+	}
+
+	public void deleteOTP(String userId, String randomNo) {
+		
+	}
+
+}
