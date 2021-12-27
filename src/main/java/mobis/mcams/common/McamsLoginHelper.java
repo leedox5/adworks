07e3e7982f0dc30755eb308bbcba8f6180873e04
Myungho Lee <leedox@naver.com>
2021-12-27 12:00:54 +0900

    SYNC

---
+++ src/main/java/mobis/mcams/common/McamsLoginHelper.java
@@ -0,0 +1,11 @@
+package mobis.mcams.common;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class McamsLoginHelper {
+
+	public static void setCookie(HttpServletResponse res, String key, String value) {
+		
+	}
+
+}
