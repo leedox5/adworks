f32eed9c321b95d322c85a29c08522faaeb73604
Myungho Lee <leedox@naver.com>
2021-12-27 11:45:34 +0900

    SYNC

---
+++ src/main/java/mobis/common/base/ServletHelper.java
@@ -0,0 +1,11 @@
+package mobis.common.base;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ServletHelper {
+
+	public static RequestInfo getRequestInfo(HttpServletRequest req) {
+		return null;
+	}
+
+}
